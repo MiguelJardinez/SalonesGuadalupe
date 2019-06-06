adb7e4c2f89d5ce2669511dde1502cb39c6c163a
Miguel <jardinez7514@hotmail.com>
2019-06-06 15:25:13 -0500

    Se agregaron flags para los navegadores en css

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,17 @@
-module.export = {
+module.exports = {
     plugins: [
-        require ('autoprefixer')({
-            grid: true,
-            background:true,
-            margin: true,
-        }),
+        // require ('autoprefixer')({
+        //     grid: true,
+        //     background:true,
+        //     margin: true,
+        // }),
         require ('postcss-cssnext')({
-            
+            features: {
+                autoprefixer: {
+                    grid: true,
+                    
+                }
+            }
         })
     ]
 }
